13439499375c3609e588d00e454dfdba6b9471a5
ShulinChen <easoningchan@gmail.com>
2014-11-04 20:15:24 -0800

    replace every meaningless wait function

--- test.js
+++ test.js
@@ -1,24 +1,20 @@
-
-
-var firstUrl;
 casper.test.begin('PulseHR feedback test', 2, function(test){
 	casper.start('http://pulse-360-dev.herokuapp.com/plugin/app', function(){
+		casper.capture('1.png');
 		this.fill('form#login',{
 			'email' : 'admin@pulsehr.net',
-			'password' : 'red' 
+			'password' : 'blue' 
 		}, true);
+		casper.cl
 	});
 
 	//make sure the page is loaded before clicking on 'login' button
 	casper.then(function(){
-		casper.wait(3000, function(){
-			// casper.capture('2.png');
-			casper.click('#btn-login-text');
-		});
+		casper.click('#btn-login-text');
 	});
 
-	// here there might be a better way to implement rather than wait for random time
-	casper.wait(3000, function(){
+	// logout button
+	casper.waitForSelector('#footer', function(){
 		test.assertExists('#logout', 'logout exists in the footer');	
 	});
 
@@ -31,30 +27,34 @@ casper.test.begin('PulseHR feedback test', 2, function(test){
 			this.sendKeys('textarea#feedback-comment', 'Good job!');
 		});
 
-		casper.wait(2000, function(){
+		casper.waitForSelector('#give-feedback', function(){
 			casper.click('#give-feedback');
 		});
-		
+
+		casper.waitForSelector('#avatar-border', function(){
+			test.assertEvalEquals(function(){
+				return __utils__.findOne('.position').textContent;
+			}, 'CEO', 'the position is right!');
+		});
+	
 		//get the href of the link we gonna jump to 
-		casper.wait(2000, function(){
-			var firstUrl = casper.getElementAttribute('#post-submit-feedback > a.btn.btn-success', 'href');	
-		});
+		// casper.then(function(){
+		// 	var firstUrl = casper.getElementAttribute('#post-submit-feedback > a.btn.btn-success', 'href');	
 
-		//click on the specific slector
-		casper.then(function() {
-			// this.click("a[href='/app#cf/wall'][data-goto = 'wall']"[0]);
-			this.click("[data-goto = 'wall'][0]");
-		});
+		// });
 
-		//make sure the selector mathch
+		// //click on the specific slector
+		// casper.then(function() {
+		// 	// this.click("a[href='/app#cf/wall'][data-goto = 'wall']"[0]);
+		// 	this.click("[data-goto = 'wall'][0]");
+		// });
 
-		casper.wait(10000, function(){
-			test.assertUrlMatch("/app#cf/wall", 'Recognition page is fine');
-		});
-		//go to the Recognition page and then test.assertUrlMatch
+		// //make sure the selector mathch
 
-		
-
+		// casper.wait(10000, function(){
+		// 	test.assertUrlMatch("/app#cf/wall", 'Recognition page is fine');
+		// });
+		// //go to the Recognition page and then test.assertUrlMatch
 		
 	}); //thenopen
 
